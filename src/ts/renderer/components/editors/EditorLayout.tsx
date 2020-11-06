e7de0a20c3debeea9557a0ca4615dcd373ee4a6b
Jed Dark <darkjedi9922@gmail.com>
2020-11-06 09:00:35 +0200

    Replace bootstrap card in editor layout with own card

--- src/ts/renderer/components/editors/EditorLayout.tsx
+++ src/ts/renderer/components/editors/EditorLayout.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
+import Card from '../Card';
 
 interface Props {
     title: string
 }
 
 const EditorLayout = function(props: React.PropsWithChildren<Props>): JSX.Element {
-    return <Card className="mb-sm-3">
-        <Card.Header>{props.title}</Card.Header>
-        <Card.Body>{props.children}</Card.Body>
-    </Card>
+    return <Card title={props.title}>{props.children}</Card>
 }
 
 export default EditorLayout;
